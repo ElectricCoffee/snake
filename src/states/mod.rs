7b8316ef2b17d9df481b39a78e0e692c57983a35
Niko Lepka <slench102+git@gmail.com>
2020-04-13 00:10:05 +0200

    add sprite sheet loader to MainState

--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,12 +1,21 @@
-use amethyst::{core::Transform, prelude::*, renderer::*};
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::Transform,
+    prelude::*,
+    renderer::formats::texture::ImageFormat,
+    renderer::*,
+};
 
-use crate::{components::*, constants::*};
+use crate::{components::*, constants::*, util::position};
 
 pub struct MainState;
 
 impl SimpleState for MainState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        let pos = position::Absolute(ARENA_CENTER_X, ARENA_CENTER_Y).to_relative();
+        let sprite_sheet = load_sprite_sheet(world);
+        world.register::<Snekment>();
 
         init_camera(world);
         //SnakeSegment::init(world, SegmentType::Head)
@@ -22,4 +31,24 @@ fn init_camera(world: &mut World) {
         .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
         .with(trans)
         .build();
+}
+
+pub fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    let texture_handle = {
+        let storage = world.read_resource::<AssetStorage<Texture>>();
+        world.read_resource::<Loader>().load(
+            "textures/sprites.png",
+            ImageFormat::default(),
+            (),
+            &storage,
+        )
+    };
+
+    let sprite_sheet = world.read_resource::<AssetStorage<SpriteSheet>>();
+    world.read_resource::<Loader>().load(
+        "textures/sprites.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet,
+    )
 }
